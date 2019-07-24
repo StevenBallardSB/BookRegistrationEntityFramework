32b062c0fa365b781aaddc0aabbe33ba1c13ba26
steven ballard <stevenballardstudios@gmail.com>
2019-07-24 14:08:22 -0700

    Added bookDB class

---
+++ BookRegistrationEntityFramework/BookDB.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRegistrationEntityFramework
+{
+    /// <summary>
+    /// Contains helper methods to manipulate book data in the database
+    /// </summary>
+    class BookDB
+    {
+        /// <summary>
+        /// Retrieves all books sorted in alphabetical by title
+        /// </summary>
+        /// <returns></returns>
+        public static List<Book> GetBooks()
+        {
+            // The using statement will force the compiler to create a try/finally . the finally will dispose of the db context
+            using(var context = new BookRegistrationEntities())
+            {
+                //LINQ Query Syntax
+                List<Book> books =
+                    (from b in context.Book
+                    orderby b.Title ascending
+                    select b).ToList();
+                return books;
+            }
+        }
+    }
+}
